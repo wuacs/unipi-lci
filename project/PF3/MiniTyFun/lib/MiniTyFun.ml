ff180127b4fe95290e09f864b555690f6b3ce295
Dario Bekic <dariobekic@outlook.it>
2024-10-26 16:15:45 +0200

    added new version of typed miniFun

--- project/PF3/MiniTyFun/lib/MiniTyFun.ml
+++ project/PF3/MiniTyFun/lib/MiniTyFun.ml
@@ -1,5 +1,5 @@
-(** The exception which is thrown by function {!type_check} *)
 exception TypeError of string
+(** The exception which is thrown by function {!type_check} *)
 
 module StringCompare = struct
   type t = string
@@ -12,7 +12,7 @@ module EnvMap = Map.Make (StringCompare)
 type var = string
 type op = Plus | Minus | And | Mul | Less | Equal
 
-type ast = 
+type ast =
   | LetFun of var * var * tau * ast * ast
   | Fun of var * tau * ast
   | Let of var * ast * ast
@@ -22,11 +22,7 @@ type ast =
   | Val of value
   | Var of var
 
-and tau = 
-  | Not_Defined
-  | Integer_t
-  | Boolean_t
-  | Closure_t of tau * tau 
+and tau = Not_Defined of var | Integer_t | Boolean_t | Closure_t of tau * tau
 
 and value =
   | Integer of int
@@ -35,52 +31,113 @@ and value =
   | Closure of var * ast * value_env
 
 and value_env = value EnvMap.t
-
 and type_env = tau EnvMap.t
 
-let type_check ast = 
-  let updateEnv var tau typing_env = EnvMap.add var tau typing_env in
-  let rec rec_type_check ast env =
-  match ast with
-    | Fun (var, tau, ast1) -> Closure_t(tau, (rec_type_check ast1 (updateEnv var tau env)))
-    | App (f, par) -> (
-        match rec_type_check f env with
-        | Closure_t (t1, t2) -> (let partype = (rec_type_check par env) in 
-                                if partype = t1 then t2 else raise (TypeError "Type checking failed"))
-        | _ -> raise (TypeError "Type checking failed"))
-    | Op (a1, op, a2) -> (
-        let a1 = rec_type_check a1 env in
-        let a2 = rec_type_check a2 env in
-        match op with
-        | Plus | Minus | Mul | Less -> (
-            match (a1, a2) with
-            | Integer_t, Integer_t -> Integer_t
-            | _ -> raise (TypeError "operands do not match binary integer operator"))
-        | And -> (
-            match (a1, a2) with
-            | Boolean_t, Boolean_t -> Boolean_t
-            | _ -> raise (TypeError "operands do not match binary boolean operator"))
-        | Equal -> (
-            match (a1, a2) with
-            | Boolean_t, Boolean_t | Integer_t, Integer_t-> Boolean_t
-            | _ -> raise (TypeError "operands do not match == operator")))
-    | If (a1, a2, a3) -> (
-        let cond = rec_type_check a1 env in
-        match cond with
-        | Boolean_t ->  let t1 = rec_type_check a2 env in 
-                        let t2 = rec_type_check a3 env in
-                        if t1 <> t2 then raise  (TypeError "Type checking failed") else t1
-        | _ -> raise ( TypeError"Type checking failed"))
-    | Let (var, a1, a2) -> rec_type_check a2 (updateEnv var (rec_type_check a1 env) env)
-    | LetFun (f, var, t, a1, a2) ->
-        rec_type_check a2 (updateEnv f (Closure_t(t, rec_type_check a1 (updateEnv var t (updateEnv f (Closure_t(t, Not_Defined)) env)))) env)
-    | Var x -> (try EnvMap.find x env with _ -> failwith ("non ho trovato " ^ x))
-    | Val x -> (match x with 
-                  | Integer _ -> Integer_t
-                  | Boolean _ -> Boolean_t
-                  | Closure(v, a, _) | RecClosure(_, v, a, _) -> 
-                    let tau_par = EnvMap.find v env in
-                    let body_par = rec_type_check a (updateEnv v tau_par env) in
-                    Closure_t(tau_par, body_par))
+let type_check ast =
+  let mergeEnv env1 env2 =
+    EnvMap.merge
+      (fun _x k1 k2 ->
+        match (k1, k2) with
+        | Some v1, Some v2 -> (
+            match v2 with
+            | Closure_t (_, Not_Defined _) -> Some v1
+            | _ ->
+                if v1 <> v2 then raise (TypeError "Type checking failed")
+                else Some v1)
+        | Some v1, None -> Some v1
+        | None, Some v2 -> Some v2
+        | None, None -> None)
+      env1 env2
   in
-  try let x = (rec_type_check ast EnvMap.empty) in Some(x) with TypeError _ -> None
+  let getOperationType operation =
+    match operation with
+    | Plus | Minus | Mul -> Integer_t
+    | Less | Equal | And -> Boolean_t
+  in
+  let getOperandType operation operand =
+    match operation with
+    | Plus | Minus | Mul | Less -> (
+        match operand with
+        | Integer_t | Not_Defined _ -> Integer_t
+        | _ -> raise (TypeError "Type checking failed"))
+    | And -> (
+        match operand with
+        | Boolean_t | Not_Defined _ -> Boolean_t
+        | _ -> raise (TypeError "Type checking failed"))
+    | Equal -> (
+        match operand with
+        (* If you do Equal(Undefined, Undefined) it doesnt pass the tc *)
+        | Not_Defined _ -> raise (TypeError "Type checking failed")
+        | x -> x)
+  in
+  let updateEnv var tau typing_env = EnvMap.add var tau typing_env in
+  let removeBinding var typing_env = EnvMap.remove var typing_env in
+  let getBinding var typing_env = EnvMap.find_opt var typing_env in
+  let getValueType value =
+    match value with
+    | Integer _ -> Integer_t
+    | Boolean _ -> Boolean_t
+    | _ -> failwith "Only integer and boolean literals allowed"
+  in
+  let rec rec_type_check : ast -> tau EnvMap.t -> tau * tau EnvMap.t =
+   fun ast env ->
+    match ast with
+    | Fun (pname, ptype, a1) ->
+        let t, re = rec_type_check a1 (updateEnv pname ptype env) in
+        (t, removeBinding pname re)
+    | LetFun (fname, pname, ptype, a1, a2) ->
+        let _, re =
+          rec_type_check a1
+            (updateEnv pname ptype
+               (updateEnv fname (Closure_t (ptype, Not_Defined fname)) env))
+        in
+        let t1, re1 = rec_type_check a2 (removeBinding pname re) in
+        (t1, removeBinding fname re1)
+    | Let (pname, a1, a2) ->
+        let t, re = rec_type_check a1 env in
+        rec_type_check a2 (updateEnv pname t (mergeEnv re env))
+    | App (a1, a2) -> (
+        let t, re = rec_type_check a2 env in
+        let t1, re1 = rec_type_check a1 (mergeEnv re env) in
+        match t1 with
+        | Closure_t (dt, Not_Defined x) ->
+            if dt <> t then raise (TypeError "Type checking failed")
+            else (Not_Defined x, re1)
+        | Closure_t (dt, ot) ->
+            if dt <> t then raise (TypeError "Type checking failed")
+            else (ot, re1)
+        | _ -> raise (TypeError "Type checking failed"))
+    | Op (a1, op, a2) ->
+        let helper : tau -> tau -> tau EnvMap.t -> op -> tau * tau EnvMap.t =
+         fun t t1 e o ->
+          match t with
+          | Not_Defined x -> (
+              let t2 = getOperandType o t1 in
+              match getBinding x e with
+              | Some (Closure_t (dt, _)) ->
+                  (t2, updateEnv x (Closure_t (dt, t2)) e)
+              | _ -> raise (TypeError "Type checking failed"))
+          | _ -> (getOperandType o t, e)
+        in
+        let t, re = rec_type_check a1 env in
+        let t1, re1 = rec_type_check a2 (mergeEnv re env) in
+        let _, re2 = helper t t1 (mergeEnv re1 env) op in
+        let _, re3 = helper t1 t re2 op in
+        (getOperationType op, re3)
+    | If (a1, a2, a3) ->
+        let t, re = rec_type_check a1 env in
+        if t <> Boolean_t then raise (TypeError "Type checking failed");
+        let t1, re1 = rec_type_check a2 (mergeEnv re env) in
+        let t2, re2 = rec_type_check a3 (mergeEnv re1 env) in
+        if t1 <> t2 then raise (TypeError "Type checking failed")
+        else (t1, mergeEnv re2 env)
+    | Val value -> (getValueType value, env)
+    | Var var -> (
+        match getBinding var env with
+        | Some t -> (t, env)
+        | None -> raise (TypeError "Type checking failed"))
+  in
+  try
+    let x = rec_type_check ast EnvMap.empty in
+    Some (fst x)
+  with TypeError _ -> None
--- project/PF3/MiniTyFun/test/MiniTyFun.ml
+++ project/PF3/MiniTyFun/test/MiniTyFun.ml
@@ -4,34 +4,104 @@ open MiniTyFun (* Assuming MiniFun is your library module *)
 (*
 Type checking 5+4
 *)
-let test_eval0 _ = 
-  let ast = Op(Val(Integer(5)), Plus, Val(Integer(4))) in
-  (match type_check ast with
-  | Some(Integer_t) -> assert_equal true true
-  | _ -> assert_failure "Sum of integers did not give an integer")
-
+let test_eval0 _ =
+  let ast = Op (Val (Integer 5), Plus, Val (Integer 4)) in
+  match type_check ast with
+  | Some Integer_t -> assert_equal true true
+  | _ -> assert_failure "Sum of integers did not give an integer"
 
 (*
 Type checking 5+((Letfun factorial n : Integer = if n == 0 then 1 else n*(factorial n - 1)) 5)
 *)
 
-let test_eval1 _ = 
-  let ast = LetFun("factorial", "n", Integer_t, If(Op(Var("n"), Equal, Val(Integer(0))), Val(Integer(1)), 
-            Op(Var("n"), Mul, App(Var("factorial"), Op(Var("n"), Minus, Val(Integer(1)))))), Op(Val(Integer(5)), Plus, App(Var("factorial"), Val(Integer(5)))))
+let test_eval1 _ =
+  let ast =
+    LetFun
+      ( "factorial",
+        "n",
+        Integer_t,
+        If
+          ( Op (Var "n", Equal, Val (Integer 0)),
+            Val (Integer 1),
+            Op
+              ( Var "n",
+                Mul,
+                App (Var "factorial", Op (Var "n", Minus, Val (Integer 1))) ) ),
+        Op (Val (Integer 5), Plus, App (Var "factorial", Val (Integer 5))) )
   in
-  (match type_check ast with
-  | Some(Integer_t) -> assert_equal true true
-  | _ -> assert_failure "5+factorial call should give integer")
+  match type_check ast with
+  | Some Integer_t -> assert_equal true true
+  | _ -> assert_failure "5+factorial call should give integer"
 
+(*
+Type checking: 
+(let function rec_fun x: Integer = 
+  let x = (rec_function 5) + 3 in
+  let y = (rec_function 3) AND true in
+  x) in (rec_function 5)
+  
+  Should fail because you use rec_function as a Integer->Integer and
+  Integer->Boolean at the same time
+*)
+
+let test_eval2 _ =
+  let body =
+    Let
+      ( "x",
+        Op (App (Var "rec_fun", Val (Integer 5)), Plus, Val (Integer 3)),
+        Let
+          ( "y",
+            Op (App (Var "rec_fun", Val (Integer 3)), And, Val (Boolean true)),
+            Var "x" ) )
+  in
+  let ast =
+    LetFun
+      ("rec_fun", "x", Integer_t, body, App (Var "rec_fun", Val (Integer 5)))
+  in
+  match type_check ast with
+  | Some _ ->
+      assert_failure
+        "Should have failed AND and + are not of interoparable domain"
+  | _ -> assert_equal true true
+
+(*
+Type checking: 
+(let function rec_fun x: Integer = 
+  let x = (rec_function 5) + 3 in
+  let y = (rec_function 3) AND true in
+  x) in (rec_function 5)
+  
+  Should fail because you use rec_function as a Integer->Integer and
+  Integer->Boolean at the same time
+*)
+
+let test_eval3 _ =
+  let body =
+    Let
+      ( "x",
+        Op (App (Var "rec_fun", Val (Integer 5)), Equal, Val (Boolean false)),
+        Let
+          ( "y",
+            Op (App (Var "rec_fun", Val (Integer 3)), And, Val (Boolean true)),
+            Var "x" ) )
+  in
+  let ast =
+    LetFun
+      ("rec_fun", "x", Integer_t, body, App (Var "rec_fun", Val (Integer 5)))
+  in
+  match type_check ast with
+  | Some _ -> assert_equal true true
+  | _ -> assert_failure "rec_fun is Integer->Boolean why not?"
 
 (* Test suite *)
 let suite =
   "MiniFun Test Suite"
   >::: [
          "summing 5 and 4... " >:: test_eval0;
-         "summing 5 and factorial(5).." >:: test_eval1
+         "summing 5 and factorial(5).." >:: test_eval1;
+         "using a type as an integer and boolean at same time..." >:: test_eval2;
+         "using a type a..." >:: test_eval3;
        ]
 
 (* Run the test suite *)
 let () = run_test_tt_main suite
-
