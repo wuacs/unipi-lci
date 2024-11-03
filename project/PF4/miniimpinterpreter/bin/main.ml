fabe0493e6253c517ba4d11fdd71f7540dcdc326
Dario Bekic <dariobekic@outlook.it>
2024-11-03 08:49:24 +0100

    delete garbage

--- project/PF4/miniimpinterpreter/bin/main.ml
+++
@@ -1,6 +0,0 @@
-open Miniimpinterpreter
-
-let () = 
-  match parse_with_errors (Lexing.from_channel stdin) with
-  | Some(x) -> (match eval x (int_of_string Sys.argv.(1)) with | Some(y) -> print_int y | _ -> print_string "error in evaluation")
-  | None -> print_string "error in parsing"
--- project/PF4/miniimpinterpreter/lib/miniimpinterpreter.mli
+++
@@ -1,5 +0,0 @@
-open MiniImp
-
-val parse_with_errors: Lexing.lexbuf -> program option
-
-val eval: program -> int -> int option
