5188cf9fc1d1d97bfc9d785bcfaa881071edf6f3
Dario Bekic <dariobekic@outlook.it>
2025-01-27 15:20:27 +0100

    refactor, moved dot producing functions to dot executable

--- project/src/bin/dfa.ml
+++ project/src/bin/dfa.ml
@@ -2,32 +2,6 @@ open Cfg
 open Miniimp
 open Data_flow_analysis
 
-let compute_live_analysis_and_create_dot () = 
-  let channel = open_in Sys.argv.(2) in
-  let filename = Sys.argv.(3) in
-  Fun.protect ~finally:(fun _ -> close_in channel) (fun _ ->
-    match parse_with_errors (Lexing.from_channel channel) with
-    | Some prog ->
-        let oc = open_out filename in
-        Fun.protect ~finally:(fun _ -> close_out oc) (fun _ ->
-          Printf.fprintf oc "%s" (live_analysis_dot (miniimp_cfg_to_minirisc (translate_miniimp prog)))
-        )
-    | None -> print_string "Error: Parsing failed\n"
-  )
-
-let compute_defined_analysis_and_create_dot () =
-  let ic = open_in Sys.argv.(2) in
-  let out_file_name = Sys.argv.(3) in
-  Fun.protect ~finally:(fun _ -> close_in ic) (fun _ ->
-    match parse_with_errors (Lexing.from_channel ic) with
-    | Some prog ->
-        let oc = open_out out_file_name in
-        Fun.protect ~finally:(fun _ -> close_out oc) (fun _ ->
-          Printf.fprintf oc "%s" (defined_analysis_dot (miniimp_cfg_to_minirisc (translate_miniimp prog)))
-        )
-    | None -> print_string "Error: Parsing failed\n"
-  )
-
 let check_for_undefinedness () =
   let ic = open_in Sys.argv.(2) in
   Fun.protect ~finally:(fun _ -> close_in ic) (fun _ ->
@@ -39,12 +13,9 @@ let check_for_undefinedness () =
   )
 
 let info_string =
-  "Usage instructions:\n\n\
-   To compute live analysis and create a DOT file:\n\
-   dune exec dfa live_analysis input_file_path output_file_path\n\n\
-   To compute defined analysis and create a DOT file:\n\
-   dune exec dfa defined_analysis input_file_path output_file_path\n\n\
-   To check for undefined variables:\n\
+  "Usage instructions:\n\n
+   To check for undefined variables in a MiniImp program and output
+   whether one such variable was found use:\n\
    dune exec dfa check_undefined input_file_path\n\n"
 
 let () =
@@ -55,12 +26,6 @@ let () =
   if (Array.length Sys.argv) <= 1 then fail_helper "No command specified" else
   let command = Sys.argv.(1) in
   match command with
-  | "live_analysis" ->
-      if Array.length Sys.argv <> 4 then fail_helper "Usage: live_analysis input_file_path output_file_prefix"
-      else compute_live_analysis_and_create_dot ()
-  | "defined_analysis" ->
-      if Array.length Sys.argv <> 4 then fail_helper "Usage: defined_analysis input_file_path output_file_prefix"
-      else compute_defined_analysis_and_create_dot ()
   | "check_undefined" ->
       if Array.length Sys.argv <> 3 then fail_helper "Usage: check_undefined input_file_path"
       else check_for_undefinedness ()
--- project/src/bin/dot.ml
+++ project/src/bin/dot.ml
@@ -1,5 +1,7 @@
 open Cfg
 open Miniimp
+open Data_flow_analysis
+
 let get_cfg_dot_of_miniimp () = 
   let ic = open_in Sys.argv.(2) in
   Fun.protect ~finally:(fun _ -> close_in ic)
@@ -55,8 +57,35 @@ let print_to_dot_interference_graph () =
         (Target_code.interference_graph_dot (Cfg.miniimp_cfg_to_minirisc (Cfg.translate_miniimp prog)));)
     | None -> failwith ("Error while parsing file " ^ namefile))
 
+let compute_live_analysis_and_create_dot () = 
+  let channel = open_in Sys.argv.(2) in
+  let filename = Sys.argv.(3) in
+  Fun.protect ~finally:(fun _ -> close_in channel) (fun _ ->
+    match parse_with_errors (Lexing.from_channel channel) with
+    | Some prog ->
+        let oc = open_out filename in
+        Fun.protect ~finally:(fun _ -> close_out oc) (fun _ ->
+          Printf.fprintf oc "%s" (live_analysis_dot (miniimp_cfg_to_minirisc (translate_miniimp prog)))
+        )
+    | None -> print_string "Error: Parsing failed\n"
+  )
+
+let compute_defined_analysis_and_create_dot () =
+  let ic = open_in Sys.argv.(2) in
+  let out_file_name = Sys.argv.(3) in
+  Fun.protect ~finally:(fun _ -> close_in ic) (fun _ ->
+    match parse_with_errors (Lexing.from_channel ic) with
+    | Some prog ->
+        let oc = open_out out_file_name in
+        Fun.protect ~finally:(fun _ -> close_out oc) (fun _ ->
+          Printf.fprintf oc "%s" (defined_analysis_dot (miniimp_cfg_to_minirisc (translate_miniimp prog)))
+        )
+    | None -> print_string "Error: Parsing failed\n"
+  )
+    
+
 let info_string =
-"This executable is for generating .Dot compliant resources.\n
+"This executable is for generating dot compliant resources.\n
 
 Every dot file obtained by one of the following invocation can be rendered visually, \
 as for example a .pdf, with the instruction:\n
@@ -65,27 +94,38 @@ as for example a .pdf, with the instruction:\n
 
 For more information refer to the Graphitz package. \n
 
-If you wish to produce a .dot compliant file containing the control flow graph \
+If you wish to produce a dot compliant file containing the control flow graph \
 of a MiniImp program then use:\n
 
 dune exec dot miniimp input_file output_file\n
 
-If you wish to produce a .dot compliant file containing the control flow graph \
+If you wish to produce a dot compliant file containing the control flow graph \
 of the MiniRISC equivalent of a MiniImp program in input use: \n
 
 dune exec dot minirisc input_file output_file\n
 
-If you wish to produce a .dot compilant file showing the interference graph of a\ 
+If you wish to produce a dot compilant file showing the interference graph of a\ 
 unoptimized translation of a MiniImp program to MiniRISC, use:
 
 dune exec dot interference input_file output_file\n
 
-If you wish to produce a .dot compliant file showing the control flow graph of a optimized\
+If you wish to produce a dot compliant file showing the control flow graph of a optimized\
 translation of a MiniImp program to MiniRISC for a target architecture with at most `registers`\ 
 number of registers then use:\n
 
-dune exec dot optimized input_file output_file registers
-"
+dune exec dot optimized input_file_path output_file_path registers
+
+If you wish to produce a dot compliant file representing the CFG of a \
+MiniImp program translated to MiniRISC where each node contains the set of registers
+live in and live out of a node, then use:\n
+
+dune exec dot liveness input_file_path output_file_path\n
+
+If you wish to produce a dot compliant file representing the CFG of a \
+MiniImp program translated to MiniRisc where each node contains the set of registers
+certainly defined at the start and after the block, then use:\n\
+
+dune exec dot undefinedness input_file_path output_file_path"
 
 let () =
   let fail_helper ?(additional_info = "Unspecified error") () =
@@ -105,7 +145,13 @@ let () =
       if Array.length Sys.argv <> 4 then
       fail_helper ~additional_info:"`interference` takes 2 parameters" () else
       print_to_dot_interference_graph ()
-  | "optimized" -> if Array.length Sys.argv > 5 then
+  | "optimized" -> if Array.length Sys.argv <> 5 then
       fail_helper ~additional_info:"`optimized` takes 3 parameters" () else
       get_optimized_riscfg_from_miniimp ()
+  | "undefinedness" -> if Array.length Sys.argv <> 4 then
+      fail_helper ~additional_info:"`undefinedness takes 2 parameters" () else
+      compute_defined_analysis_and_create_dot ()
+  | "liveness" ->  if Array.length Sys.argv <> 4 then
+      fail_helper ~additional_info:"`liveness takes 2 parameters`" () else
+      compute_live_analysis_and_create_dot ()
   | _ -> fail_helper ()
